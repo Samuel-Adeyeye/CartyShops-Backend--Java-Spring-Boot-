8e981863f2b43afd5cf03ff791896a73a9d0c442
Samuel <36992355+Samuel-Adeyeye@users.noreply.github.com>
2025-01-31 05:22:18 +0100

    init commit

---
+++ src/main/java/com/samueladeyeye/cartyshops/CartyShopsApplication.java
@@ -0,0 +1,12 @@
+package com.samueladeyeye.cartyshops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartyShopsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CartyShopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samueladeyeye/cartyshops/CartyShopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samueladeyeye.cartyshops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartyShopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
